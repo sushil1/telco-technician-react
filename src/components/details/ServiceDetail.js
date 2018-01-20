83d0108e2a36a6e32940b0383cd77a2a18e14b4e
Sushil L <sushil_samson@hotmail.com>
2018-01-20 12:22:12 +1100

    service detail service area added

--- src/components/details/ServiceDetail.js
+++ src/components/details/ServiceDetail.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Header, Divider, Icon, Grid, List, Button } from 'semantic-ui-react';
+import { Header, Divider, Icon, Grid, List, Button, Segment, Label} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import ReactImageFallback from 'react-image-fallback'
 
 const ServiceDetail = ({ service }) => (
-	<Grid.Row>
+	<Grid.Row style={{marginTop:'30px'}}>
 	<Grid.Column mobile={16} tablet={8} computer={8}>
 		<Header as="h2">
 			<Header.Content>{service.name}</Header.Content>
 		</Header>
-		<Button animated tabIndex="0" as={Link} to={`/book/${service._id}`}>
-			<div className="visible content">Book a technician</div>
-			<div className="hidden content">{service.name}</div>
-		</Button>
+
 		<Header sub>Description</Header>
 		<Divider />
 		<p>{service.description}</p>
@@ -69,15 +66,55 @@ const ServiceDetail = ({ service }) => (
 		<Header sub>Our stats</Header>
 		<Divider />
 	</Grid.Column>
-	<Grid.Column mobile={16} tablet={8} computer={8} centered style={{paddingTop:'10px'}}>
+	<Grid.Column mobile={0} tablet={2} computer={2}>
+	</Grid.Column>
+	<Grid.Column mobile={16} tablet={6} computer={6} style={{marginTop:'30px'}}>
+
+
+		<Segment  fluid>
 		<ReactImageFallback
 					src={service.thumbnail}
-					fallbackImage='http://via.placeholder.com/250x250'
+					fallbackImage='http://via.placeholder.com/450x350'
+					alt='Thumbnail'
+					className='ui image'
+				/>
+		<br />
+			<Button color='teal' fluid as={Link} to={`/book/${service._id}`}>
+				Book {service.name}
+			</Button>
+
+		</Segment>
+
+
+		<Segment  fluid>
+
+
+		<Header as='h5'>
+				Service Areas
+			</Header>
+		<ReactImageFallback
+					src={service.thumbnail}
+					fallbackImage='https://upload.wikimedia.org/wikipedia/commons/2/2f/Australia_location_map.svg'
 					alt='Thumbnail'
 					className='ui image'
 				/>
 
-	</Grid.Column>
+			<Segment inverted >
+			<Header as='h5'>Currently Available in Sydney Suburbs</Header>
+			<Label.Group>
+		    <Label as='a'>Northern Beaches</Label>
+		    <Label as='a'>Inner West </Label>
+		    <Label as='a'>Western </Label>
+		    <Label as='a'>Eastern </Label>
+		    <Label as='a'>Northern</Label>
+		  </Label.Group>
+			</Segment>
+		</Segment>
+
+</Grid.Column>
+
+
+
 
 	</Grid.Row>
 );
